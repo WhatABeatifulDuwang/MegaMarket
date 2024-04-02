fffbb185c4d386a3bb04f7bd1a23b2e78fca4641
Quintenvddb <QBsmartguy@gmail.com>
2024-04-02 21:44:41 +0200

    Product page initial and small home page update

--- Code/Assets/index-script.js
+++ Code/Assets/index-script.js
@@ -2,6 +2,10 @@ function gotoLoginPage() {
     window.location.href = 'login.php';
 }
 
+function gotoProductPage() {
+    window.location.href = 'product-page.html'; 
+}
+
 function toggleShoppingCart() {
     alert("Yup hier moet nog een shopping cart gedeelte komen")
 }
