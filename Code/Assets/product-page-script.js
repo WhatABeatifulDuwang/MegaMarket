b9defdd91998dc0147430a2eada724437a348396
Quintenvddb <QBsmartguy@gmail.com>
2024-04-07 14:29:40 +0200

    Product page styling finished and small index page style update

---
+++ Code/Assets/product-page-script.js
@@ -0,0 +1,11 @@
+function gotoLoginPage() {
+    window.location.href = 'login.php';
+}
+
+function gotoProductPage() {
+    window.location.href = 'product-page.html';
+}
+
+function toggleShoppingCart() {
+    alert("Yup hier moet nog een shopping cart gedeelte komen")
+}
